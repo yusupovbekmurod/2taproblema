5647111355fe319117a105e6cfd92fc809c2f419
yusupovbekmurod <bekmurodyusupov81@gmail.com>
2022-08-27 09:16:41 +0500

    2_ta_masala

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//  ========== 1-masala ==========
+
+// let array = [1, 3, 5, 9];
+// let sum = 0;
+
+// for (let i = 0; i < array.length; i++) {
+//   sum += array[i];
+// }
+// console.log(sum);
+
+//  ========== 2-masala ==========
+
+// let ochqorin = +prompt("Qancha puling bor: ");
+
+// if (ochqorin > 50 && ochqorin < 300) {
+//   console.log(ochqorin + ochqorin * 0.15);
+// } else if (ochqorin < 50  ) {
+//   console.log(ochqorin + (ochqorin * 0.2));
+// }else if(ochqorin > 300){
+//    console.log(ochqorin + (ochqorin * 0.2))
+// }
